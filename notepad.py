968c72b02e2355668e74a73e7cb64768c9edf61e
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-06-30 10:39:15 -0600

    update notepad.py with ch 3.22

--- notepad.py
+++ notepad.py
@@ -715,3 +715,60 @@ class Sorter:
 sorter = Sorter(group)
 numbers.sort(key=sorter)
 assert sorter.found is True
+
+## Item 22: Reduce Visual Noise with Variable Positional Arguments
+"""
+Functions can accept a variable number of positional arguments by using
+*args in the def statement.
+
+You can use the items from a sequence as the positional arguments for a function 
+with the * operator
+
+Using the * operator with a generator may cause a program to run out of memory and crash
+
+Adding new positional parameters to functions that accept *args can introduce
+hard to detect bugs
+"""
+# Example w/o variable positional arguments
+def log(message, values):
+    if not values:
+        print(message)
+    else:
+        values_str = ', '.join(str(x) for x in values)
+        print(f'{message}: {values_str}')
+log('My numbers are', [1, 2])
+log('Hi there', [])  # Need to define an empty list, not ideal!
+
+# Example w/ variable positional arguments
+def log(message, *values):  # The only difference
+    if not values:
+        print(message)
+    else:
+        values_str = ', '.join(str(x) for x in values)
+        print(f'{message}: {values_str}')
+log('My numbers are', 1, 2)
+log('Hi there')  # Much better
+
+# Example using items from a sequence as the positional argument
+favorites = [7, 33, 99]
+log('Favorite colors', *favorites)
+
+# Example of using the * operator with a generator
+def my_generator():
+    for i in range(10):
+        yield i
+def my_func(*args):
+    print(args)
+it = my_generator()
+my_func(*it)  # Iterates through the generator until it's exhausted, could be bad!
+
+# Example of new positional parameters creating issues!
+def log(sequence, message, *values):
+    if not values:
+        print(f'{sequence} - {message}')
+    else:
+        values_str = ', '.join(str(x) for x in values)
+        print(f'{sequence} - {message}: {values_str}')
+log(1, 'Favorites', 7, 33)      # New with *args OK
+log(1, 'Hi there')              # New message only OK
+log('Favorite numbers', 7, 33)  # Old usage breaks
