3c02d650eee212214584e125e9298e41475df0aa
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-06-30 11:14:21 -0600

    update notepad.py with ch 3.24

--- notepad.py
+++ notepad.py
@@ -824,3 +824,100 @@ weight_diff = 0.5
 time_diff = 3
 pounds_per_hour = flow_rate(weight_diff, time_diff,
                             period=3600, units_per_kg=2.2)
+
+## Item 24: Use None and Docstrings to Specify Dynamic Default Arguments
+"""
+A default argument value is evaluated only once: during function definition at module
+load time. This can cause odd behaviours for dynamic values (e.g., {}, [] or datetime.now())
+
+Use None as the default value for any keyword argument that has a dynamic value. Document
+the actual default behaviour in the function's docstring.
+
+Using None to represent keyword argument default values also works correctly with type
+annotations.
+"""
+# How dynamic default argument values can create issues
+from time import sleep
+from datetime import datetime
+def log(message, when=datetime.now()):
+    print(f'{when}: {message}')
+
+# These two calls to log will return the same datetime
+log('Hi there!')
+sleep(0.1)
+log('Hello again!')
+
+# This is because the default argument value is evaluated only once!
+
+# Use None as the default value to resolve this issue
+
+def log(message, when=None):
+    """Log a message with a timestamp.
+    Args:
+        message: Message to print.
+        when: datetime of when the message occurred.
+            Defaults to the present time.
+    """
+    if when is None:
+        when = datetime.now()
+    print(f'{when}: {message}')
+
+# These two calls to log will now return different datetimes
+log('Hi there!')
+sleep(0.1)
+log('Hello again!')
+
+# How dynamic default argument values can create issues example 2
+import json
+
+def decode(data, default={}):
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+foo = decode('bad data')
+foo['stuff'] = 5
+bar = decode('also bad')
+bar['meep'] = 1
+
+# These return the same dictionary because they reference the same default {}
+print('Foo:', foo)
+print('Bar:', bar)
+
+# Use None as the default value to resolve this issue
+def decode(data, default=None):
+    """Load JSON data from a string.
+    Args:
+        data: JSON data to decode.
+        default: Value to return if decoding fails.
+            Defaults to an empty dictionary.
+    """
+    try:
+        return json.loads(data)
+    except ValueError:
+        if default is None:
+            default = {}  # Default is now run when the function is called
+        return default
+
+foo = decode('bad data')
+foo['stuff'] = 5
+bar = decode('also bad')
+bar['meep'] = 1
+print('Foo:', foo)
+print('Bar:', bar)
+assert foo is not bar
+
+# Type annotations
+from typing import Optional
+def log_typed(message: str,
+              when: Optional[datetime]=None) -> None:
+    """Log a message with a timestamp.
+    Args:
+        message: Message to print.
+        when: datetime of when the message occurred.
+            Defaults to the present time.
+    """
+    if when is None:
+        when = datetime.now()
+    print(f'{when}: {message}')
