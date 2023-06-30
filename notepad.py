fae7e8fab74fb187cebf1081c42922950320171d
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-06-30 11:52:05 -0600

    update notepad.py with ch 3.26

--- notepad.py
+++ notepad.py
@@ -1003,3 +1003,64 @@ result = safe_division_e(22, 7, 5)
 print(result)
 result = safe_division_e(22, 7, ndigits=2)
 print(result)
+
+## Item 26: Define Function Decorators with functools.wraps
+"""
+Decorators in Python are syntax to allow one function to modify another function
+at runtime.
+
+Using decorators can cause strange behaviours in tools that do introspection, such
+as debuggers.
+
+Use the wraps decorator from the functools built-in module when you define your
+own decorators to avoid issues.
+"""
+# Example decorator w/o wraps
+def trace(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'{func.__name__}({args!r}, {kwargs!r}) '
+              f'-> {result!r}')
+        return result
+    return wrapper
+
+# Call the decorator using the @ symbol
+@trace
+def fibonacci(n):
+    """Return the n-th Fibonacci number"""
+    if n in (0, 1):
+        return n
+    return (fibonacci(n - 2) + fibonacci(n - 1))
+
+# Call the decorator by wrapping it with the intended function
+fibonacci = trace(fibonacci)
+
+# Decorated function
+fibonacci(4)
+
+# Issue with introspection!
+print(fibonacci)
+help(fibonacci)
+
+# Resolve the issue with introspection by using wraps
+from functools import wraps
+
+def trace(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'{func.__name__}({args!r}, {kwargs!r}) '
+              f'-> {result!r}')
+        return result
+    return wrapper
+
+@trace
+def fibonacci(n):
+    """Return the n-th Fibonacci number"""
+    if n in (0, 1):
+        return n
+    return (fibonacci(n - 2) + fibonacci(n - 1))
+
+# Issue resolved :)
+print(fibonacci)
+help(fibonacci)
