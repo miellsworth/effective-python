bcfd15f19e5a441ef3d2a188f75bdbcf97dd90ca
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-08-18 14:21:16 -0600

    update notepad.py with item 32

--- notepad.py
+++ notepad.py
@@ -1394,3 +1394,45 @@ print(next(it))
 # Use a generator as an input to a generator expression
 roots = ((x, x**0.5) for x in it)
 print(next(roots))
+
+## Item 33: Compose Multiple Generators with yield from
+"""
+- The yield from expression allows you to compose multiple nested
+generators together into a single combined generator.
+- yield from provides better performance than manually iterating
+nested generators and yielding their outputs.
+"""
+# Set-up example
+def move(period, speed):
+    for _ in range(period):
+        yield speed
+def pause(delay):
+    for _ in range(delay):
+        yield 0
+
+# A bit repetitive...
+def animate():
+    for delta in move(4, 5.0):
+        yield delta
+    for delta in pause(3):
+        yield delta
+    for delta in move(2, 3.0):
+        yield delta
+
+def render(delta):
+    print(f'Delta: {delta:.1f}')
+    # Move the images onscreen
+
+def run(func):
+    for delta in func():
+        render(delta)
+
+run(animate)
+
+# Remove the repetition with yield from and speed things up!
+def animate_composed():
+    yield from move(4, 5.0)
+    yield from pause(3)
+    yield from move(2, 3.0)
+
+run(animate_composed)
