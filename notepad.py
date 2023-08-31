10dc6d79ada7e20b64e92be1747a2f91daa49e80
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-08-31 10:27:03 -0600

    update notepad with item 37

--- notepad.py
+++ notepad.py
@@ -1629,3 +1629,160 @@ you need the flexibility of a full class.
 internal state dictionaries get complicated.
 """
 
+# Set-up example
+class SimpleGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = []
+
+    def report_grade(self, name, score):
+        self._grades[name].append(score)
+
+    def average_grade(self, name):
+        grades = self._grades[name]
+        return sum(grades) / len(grades)
+
+book = SimpleGradebook()
+book.add_student('Isaac Newton')
+book.report_grade('Isaac Newton', 90)
+book.report_grade('Isaac Newton', 95)
+book.report_grade('Isaac Newton', 85)
+print(book.average_grade('Isaac Newton'))
+
+# Modify class to include inner dictionary to log grades by subject
+from collections import defaultdict
+
+class BySubjectGradebook:
+    def __init__(self):
+        self._grades = {} # Outer dict
+    
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list) # Inner dict
+
+    # New methods to account for subject
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'Gym', 90)
+book.report_grade('Albert Einstein', 'Gym', 95)
+print(book.average_grade('Albert Einstein'))
+
+# Modify class to change the weighting of each grade
+class WeightedGradebook:
+    def __init__(self):
+        self._grades = {}
+    
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append((score, weight))
+
+    # Nested for loops start to make this code complicated
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+
+        score_sum, score_count = 0, 0
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+            
+            score_sum += subject_avg / total_weight
+            score_count += 1
+
+        return score_sum / score_count
+
+book = WeightedGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75, 0.05)
+book.report_grade('Albert Einstein', 'Math', 65, 0.15)
+book.report_grade('Albert Einstein', 'Math', 70, 0.80)
+book.report_grade('Albert Einstein', 'Gym', 100, 0.40)
+book.report_grade('Albert Einstein', 'Gym', 85, 0.60)
+print(book.average_grade('Albert Einstein'))
+
+# Refactoring to simplify and prevent nested dictionaries using Classes
+## Start with a simple tuple to capture grade and weight
+grades = []
+grades.append((95, 0.45))
+grades.append((85, 0.55))
+total = sum(score * weight for score, weight in grades)
+total_weight = sum(weight for _, weight in grades)
+average_grade = total / total_weight
+
+## Including additional information starts to complicate things
+grades = []
+grades.append((95, 0.45, 'Great job'))
+grades.append((85, 0.55, 'Better next time'))
+total = sum(score * weight for score, weight, _ in grades)
+total_weight = sum(weight for _, weight, _ in grades)
+average_grade = total / total_weight
+
+## Using named tuples resolves some issues
+from collections import namedtuple
+Grade = namedtuple('Grade', ('score', 'weight'))
+
+# Use a class to represent a single subject that contains a set of grades
+class Subject:
+    def __init__(self):
+        self._grades = []
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+# Use a class to represent a set of subjects tied to a particular student
+class Student:
+    def __init__(self):
+        self._subjects = defaultdict(Subject)
+    def get_subject(self, name):
+        return self._subjects[name]
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
+
+# Use a class to create a container for all students
+class Gradebook:
+    def __init__(self):
+        self._students = defaultdict(Student)
+    def get_student(self, name):
+        return self._students[name]
+
+book = Gradebook()
+albert = book.get_student('Albert Einstein')
+math_albert = albert.get_subject('Math')
+math_albert.report_grade(75, 0.05)
+math_albert.report_grade(65, 0.15)
+math_albert.report_grade(70, 0.80)
+gym_albert = albert.get_subject('Gym')
+gym_albert.report_grade(100, 0.40)
+gym_albert.report_grade(85, 0.60)
+print(albert.average_grade())
