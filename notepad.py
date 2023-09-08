3be95439d372b9ed946d11fa2113bb16a6149e32
Michael Ellsworth <michael.ellsworth7@gmail.com>
2023-09-08 10:19:11 -0600

    update notepad with item 43

--- notepad.py
+++ notepad.py
@@ -2448,3 +2448,133 @@ class Child(ApiClass):
 
 a = Child()
 print(f'{a.get()} and {a._value} are different')
+
+## Item 43: Inherit from collections.abc for Custom Container Types
+"""
+- Inherit directly from Python's container types (like list or dict) for
+simple use cases.
+- Beware of the large number of methods required to implement custom
+container types correctly.
+- Have your custom container types inherit from the interfaces
+defined in collections.abc to ensure that your classes match
+required interfaces and behaviors.
+"""
+
+# Custom list type for counting frequency
+class FrequencyList(list):
+    def __init__(self, members):
+        super().__init__(members)
+    def frequency(self):
+        counts = {}
+        for item in self:
+            counts[item] = counts.get(item, 0) + 1
+        return counts
+
+foo = FrequencyList(['a', 'b', 'a', 'c', 'b', 'a', 'd'])
+print('Length is', len(foo))
+
+foo.pop()
+print('After pop:', repr(foo))
+print('Frequency:', foo.frequency())
+
+# Object that allows indexing (similar to a list) but isn't a list subclass
+class BinaryNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+## Python containers have instance methods that have special names
+bar = [1, 2, 3]
+bar[0]  # interpreted as bar.__getitem__(0)
+
+# Create a custom implementation of __getitem__
+class IndexableNode(BinaryNode):
+    def _traverse(self):
+        if self.left is not None:
+            yield from self.left._traverse()
+        yield self
+        if self.right is not None:
+            yield from self.right._traverse()
+    def __getitem__(self, index):
+        for i, item in enumerate(self._traverse()):
+            if i == index:
+                return item.value
+        raise IndexError(f'Index {index} is out of range')
+
+tree = IndexableNode(
+    10,
+    left=IndexableNode(
+        5,
+        left=IndexableNode(2),
+        right=IndexableNode(
+            6,
+            right=IndexableNode(7))),
+    right=IndexableNode(
+        15,
+        left=IndexableNode(11)))
+
+# Access of binary tree elements is similar to a list now with __getitem__ implementation
+print('LRR is', tree.left.right.right.value)
+print('Index 0 is', tree[0])
+print('Index 1 is', tree[1])
+print('11 in the tree?', 11 in tree)
+print('17 in the tree?', 17 in tree)
+print('Tree is', list(tree))
+
+# Not all sequence semantics are available though
+len(tree)  # throws an error because we would require the __len__ special method
+
+# Create the __len__ special method
+class SequenceNode(IndexableNode):
+    def __len__(self):
+        for count, _ in enumerate(self._traverse(), 1):
+            pass
+        return count
+
+tree = SequenceNode(
+    10,
+    left=SequenceNode(
+        5,
+        left=SequenceNode(2),
+        right=SequenceNode(
+            6,
+            right=SequenceNode(7))),
+    right=SequenceNode(
+        15,
+        left=SequenceNode(11))
+)
+
+print('Tree length is', len(tree))
+
+# Defining these special methods is cumbersome, especially when they are expected
+# Instead, we can use the abstract base classes available in collections.abc
+# These provide all of the typical methods for each container type
+
+from collections.abc import Sequence
+
+class BadType(Sequence):
+    pass
+
+foo = BadType()  # throws an error because you forgot to implement required methods
+
+# Inheriting SequenceNode gives you the required __getitem__ and __len__ methods
+# Additional methods like index and count now come with it for free!
+class BetterNode(SequenceNode, Sequence):
+    pass
+
+tree = BetterNode(
+    10,
+    left=BetterNode(
+        5,
+        left=BetterNode(2),
+        right=BetterNode(
+            6,
+            right=BetterNode(7))),
+    right=BetterNode(
+        15,
+        left=BetterNode(11))
+)
+
+print('Index of 7 is', tree.index(7))
+print('Count of 10 is', tree.count(10))
